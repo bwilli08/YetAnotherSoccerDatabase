33aff28c325970ec9ed94e32d8d04e23d9743c38
Brent Williams <brent.williams333@gmail.com>
2018-03-04 22:01:11 -0800

    Formatting and country backfill

--- new_configuration/database.sql
+++ new_configuration/database.sql
@@ -1,145 +1,144 @@
-#DROP DATABASE IF EXISTS SportsMonksDB;
+DROP DATABASE IF EXISTS SportsMonksDB;
 
-#CREATE DATABASE FootballDataDB
-#  DEFAULT CHARACTER SET utf8
-#  DEFAULT COLLATE utf8_general_ci;
+CREATE DATABASE SportsMonksDB
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
 
 USE SportsMonksDB;
 
 # Country API - Include leagues
 CREATE TABLE Country (
-  id          INTEGER PRIMARY KEY,
-  name        VARCHAR(64),
-  continent   VARCHAR(16)
+  id        INTEGER PRIMARY KEY,
+  name      VARCHAR(64),
+  continent VARCHAR(16)
 );
 
 # Venue API - Populate on demand as we go through Clubs
 CREATE TABLE Venue (
-  id INTEGER PRIMARY KEY,
+  id   INTEGER PRIMARY KEY,
   name VARCHAR(64),
   city VARCHAR(64)
 );
 
 # Inferred?
 CREATE TABLE Position (
-  id INTEGER PRIMARY KEY,
+  id   INTEGER PRIMARY KEY,
   name VARCHAR(32)
 );
 
 # Leagues API - include country and seasons
 CREATE TABLE Competition (
-  id                  INTEGER PRIMARY KEY,
-  is_cup              BOOLEAN,
-  country_id          INTEGER,
-  name                VARCHAR(64),
-  finished_backfill   BOOLEAN,
+  id                INTEGER PRIMARY KEY,
+  is_cup            BOOLEAN,
+  country_id        INTEGER,
+  name              VARCHAR(64),
+  finished_backfill BOOLEAN,
 
-  FOREIGN KEY (country_id) REFERENCES Country(id)
+  FOREIGN KEY (country_id) REFERENCES Country (id)
 );
 
 # Season API - include league
 CREATE TABLE Season (
-  id          INTEGER PRIMARY KEY,
-  year        VARCHAR(12),
-  league_id   INTEGER,
+  id        INTEGER PRIMARY KEY,
+  year      VARCHAR(12),
+  league_id INTEGER,
 
-  FOREIGN KEY (league_id) REFERENCES Competition(id)
+  FOREIGN KEY (league_id) REFERENCES Competition (id)
 );
 
 # Teams API - include venue and country
 CREATE TABLE Club (
-  id INTEGER PRIMARY KEY,
-  name  VARCHAR(64),
-  country_id  INTEGER,
+  id               INTEGER PRIMARY KEY,
+  name             VARCHAR(64),
+  country_id       INTEGER,
   is_national_team BOOLEAN,
-  venue_id  INTEGER,
+  venue_id         INTEGER,
 
-  FOREIGN KEY (country_id) REFERENCES Country(id),
-  FOREIGN KEY (venue_id) REFERENCES Venue(id)
+  FOREIGN KEY (country_id) REFERENCES Country (id),
+  FOREIGN KEY (venue_id) REFERENCES Venue (id)
 );
 
 # Fixture API - populate on-demand
 CREATE TABLE ClubSeason (
   season_id INTEGER,
-  club_id INTEGER,
+  club_id   INTEGER,
 
   PRIMARY KEY (season_id, club_id),
-  FOREIGN KEY (season_id) REFERENCES Season(id),
-  FOREIGN KEY (club_id) REFERENCES Club(id)
+  FOREIGN KEY (season_id) REFERENCES Season (id),
+  FOREIGN KEY (club_id) REFERENCES Club (id)
 );
 
 # Player API - include position
 # Have to retrieve by player id, so populate this on-demand (as we go through fixtures?)
 CREATE TABLE Player (
-  id INTEGER PRIMARY KEY,
+  id          INTEGER PRIMARY KEY,
   nationality VARCHAR(64),
   position_id INTEGER,
-  name  VARCHAR(64),
-  birthdate DATE,
-  height  INTEGER,
-  weight  INTEGER,
+  name        VARCHAR(64),
+  birthdate   DATE,
+  height      INTEGER,
+  weight      INTEGER,
 
-  FOREIGN KEY (position_id) REFERENCES Position(id)
+  FOREIGN KEY (position_id) REFERENCES Position (id)
 );
 
 # Fixture API - Pass in the specific competition that I care about
 CREATE TABLE Fixture (
-  id INTEGER PRIMARY KEY,
-  season_id INTEGER,
-  venue_id INTEGER,
-  league_id INTEGER,
-  home_team_id INTEGER,
-  away_team_id INTEGER,
-  date_of_game DATETIME,
+  id              INTEGER PRIMARY KEY,
+  season_id       INTEGER,
+  venue_id        INTEGER,
+  home_team_id    INTEGER,
+  away_team_id    INTEGER,
+  date_of_game    DATETIME,
 
   # A bunch of stats
   home_team_score INTEGER,
   away_team_score INTEGER,
 
-  FOREIGN KEY (season_id) REFERENCES Season(id),
-  FOREIGN KEY (venue_id) REFERENCES Venue(id),
-  FOREIGN KEY (league_id, home_team_id) REFERENCES ClubSeason(league_id, club_id),
-  FOREIGN KEY (league_id, away_team_id) REFERENCES ClubSeason(league_id, club_id)
+  FOREIGN KEY (season_id) REFERENCES Season (id),
+  FOREIGN KEY (venue_id) REFERENCES Venue (id),
+  FOREIGN KEY (season_id, home_team_id) REFERENCES ClubSeason (season_id, club_id),
+  FOREIGN KEY (season_id, away_team_id) REFERENCES ClubSeason (season_id, club_id)
 );
 
 # Fixture API - Include lineups and substitutions
 CREATE TABLE PlayerGame (
-  player_id INTEGER,
-  fixture_id INTEGER,
-  club_id INTEGER,
-  position VARCHAR(8),
+  player_id        INTEGER,
+  fixture_id       INTEGER,
+  club_id          INTEGER,
+  position         VARCHAR(8),
 
-  started BOOLEAN,
-  minutes_played INTEGER,
+  started          BOOLEAN,
+  minutes_played   INTEGER,
 
-  goals_scored INTEGER,
-  goals_conceded INTEGER,
-  assists INTEGER,
+  goals_scored     INTEGER,
+  goals_conceded   INTEGER,
+  assists          INTEGER,
 
-  shots_on_goal INTEGER,
-  shots_total INTEGER,
+  shots_on_goal    INTEGER,
+  shots_total      INTEGER,
 
-  fouls_committed INTEGER,
-  fouls_drawn INTEGER,
+  fouls_committed  INTEGER,
+  fouls_drawn      INTEGER,
 
-  interceptions INTEGER,
-  saves INTEGER,
-  clearances INTEGER,
-  tackles INTEGER,
-  offsides INTEGER,
-  blocks INTEGER,
+  interceptions    INTEGER,
+  saves            INTEGER,
+  clearances       INTEGER,
+  tackles          INTEGER,
+  offsides         INTEGER,
+  blocks           INTEGER,
 
-  pen_saved INTEGER,
-  pen_missed INTEGER,
-  pen_scored INTEGER,
+  pen_saved        INTEGER,
+  pen_missed       INTEGER,
+  pen_scored       INTEGER,
 
-  passes_total INTEGER,
-  passes_accuracy INTEGER,
-  crosses_total INTEGER,
+  passes_total     INTEGER,
+  passes_accuracy  INTEGER,
+  crosses_total    INTEGER,
   crosses_accuracy INTEGER,
 
   PRIMARY KEY (player_id, fixture_id),
-  FOREIGN KEY (player_id) REFERENCES Player(id),
-  FOREIGN KEY (fixture_id) REFERENCES Fixture(id),
-  FOREIGN KEY (club_id) REFERENCES Club(id)
+  FOREIGN KEY (player_id) REFERENCES Player (id),
+  FOREIGN KEY (fixture_id) REFERENCES Fixture (id),
+  FOREIGN KEY (club_id) REFERENCES Club (id)
 );
--- scripts/sports_monks.py
+++ scripts/sports_monks.py
@@ -1,17 +1,106 @@
 #!/usr/bin/python3
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-import json
+import sys
+import pandas
 import requests
+from sqlalchemy import create_engine
 
+sys.path.insert(0, '../internal')
+import sports_monks_tok
+import databaseinfo
+
+##### Global variables
+api_token = sports_monks_tok.sports_monks_token()
+token_param = {'api_token': api_token}
 headers = {'Accept': 'application/json'}
-params = {'include': 'fixtures', 'api_token': 'FaoR0NXWoaCC0r1JEHVhLOudUTrFcacax14DQw4S3T8aI4dRFc6u7gwJ8rjH'}
-r = requests.get("https://soccer.sportmonks.com/api/v2.0/seasons", headers=headers, params=params)
+base_url = "https://soccer.sportmonks.com/api/v2.0"
 
-print(json.dumps(r.json(), indent=2))
+##### Global Engine Object
+engine = create_engine('mysql+mysqlconnector://{}:{}@{}/{}'.format(
+    databaseinfo.db_user(),
+    databaseinfo.db_passwd(),
+    databaseinfo.db_host(),
+    databaseinfo.db_name()))
+
+
+def convert_sql_result_to_list(statement, fun):
+    return list(map(fun, engine.execute(statement).fetchall()))
+
+
+def convert_sql_result_to_set(statement, fun):
+    return set(map(fun, engine.execute(statement).fetchall()))
+
+
+# Global State of Database Tables (Only PKs)
+countries = convert_sql_result_to_set("SELECT id FROM Country", (lambda x: x[0]))
+
+
+##### Helper Functions
+def merge_dicts(x, y):
+    if not x:
+        return y
+    elif not y:
+        return x
+    else:
+        z = x.copy()   # start with x's keys and values
+        z.update(y)    # modifies z with y's keys and values & returns None
+        return z
+
+def sports_monks_api(api_string, additional_params):
+    params = merge_dicts(token_param, additional_params)
+    return requests.get(base_url + api_string, headers=headers, params=params).json()
+
+def paginated_request(api_endpoint, base_params, data_func):
+    additional_params = base_params
+    cont = True
+
+    while cont:
+        resp = sports_monks_api(api_endpoint, additional_params)
+
+        links = resp['meta']['pagination']['links']
+        if 'next' in links:
+            additional_params = base_params
+            page_params = links['next'].split('?')[1:]
+            for param in page_params:
+                (key, val) = tuple(param.split('='))
+                additional_params = merge_dicts(additional_params, {key: val})
+        else:
+            cont = False
+
+        data_func(resp['data'])
+
+
+##### Dictionaries
+country_dict = {
+    'id': [],
+    'name': [],
+    'continent': []
+}
+
+
+##### Workflow Methods
+def parse_country_data(data_list):
+    for country in data_list:
+        id = country['id']
+        name = country['name']
+        continent = "None"
+        if 'continent' in country:
+            continent = country['continent']
+        elif country['extra']:
+            extra = country['extra']
+            if 'continent' in extra:
+                continent = extra['continent']
+
+        if id not in countries:
+            country_dict['id'].append(id)
+            country_dict['name'].append(name)
+            country_dict['continent'].append(continent)
 
 def populate_countries():
-    print()
+    paginated_request("/countries", None, parse_country_data)
+    pandas.DataFrame(country_dict).to_sql("Country", con=engine, if_exists='append', index=False)
+
 
 def populate_competitions():
     print()
@@ -36,3 +125,5 @@ def add_player_game(game_json, started):
 
 def populate_fixtures():
     print()
+
+populate_countries()
