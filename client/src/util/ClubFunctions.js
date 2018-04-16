91b597edfc27d61e5791e94a06f8ec48a259141e
Brent Williams <brent.williams333@gmail.com>
2018-04-16 10:16:12 -0700

    Formatting

--- client/src/util/ClubFunctions.js
+++ client/src/util/ClubFunctions.js
@@ -3,7 +3,7 @@ import Client from "../Client";
 export var clubs = [];
 Client.club_search(result => clubs = result);
 
-export function find_club_by_id(id)  {
+export function find_club_by_id(id) {
     for (var i = 0; i < clubs.length; i++) {
         if (clubs[i].id === id) {
             return clubs[i];
